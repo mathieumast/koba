164f8e5a6cb1a4a943face535b4e86111810222d
Mathieu MAST <dev@sigvmmt>
2014-05-19 23:36:59 +0200

    Demo model

--- src/demo/model/demo.js
+++ src/demo/model/demo.js
@@ -1,13 +1,17 @@
-var myModel = new Backbone.Model({
-    firstName: "Planet",
-    lastName: "Earth"
-});
-var MyView = koba.View.extend({
-    el: '#content',
-    render: function() {
-        this.$el.html("<div id='my-view' data-bind='template: {name: \"tmpl\"}'></div>");
-        myView.bindData();
-    }
-});
-var myView = new MyView({data: myModel});
-myView.render();
+window.onload = function() {
+
+    var myModel = new Backbone.Model({
+        firstName: "Planet",
+        lastName: "Earth"
+    });
+    var MyView = koba.View.extend({
+        el: '#content',
+        render: function() {
+            this.$el.html("<div id='my-view' data-bind='template: {name: \"tmpl\"}'></div>");
+            myView.bindData();
+        }
+    });
+    var myView = new MyView({data: myModel});
+    myView.render();
+
+}
